e05102f9b90333d08bab481ffa4e9ada5fd130d5
王永辉 <39959340+yonghui-wang@users.noreply.github.com>
2021-11-29 16:46:16 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    target: 'modules', // 默认值
+    // sourcemap: true,
+  },
+  server: {
+    port: 8080,
+    host: '0.0.0.0',
+    proxy: {
+      '/api/': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (p) => p.replace(/^\/api/, ''),
+      },
+      '/api-prod/': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: (p) => p.replace(/^\/api-prod/, ''),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+    },
+  },
+})
